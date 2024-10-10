a48e636893f6a3549035edc00674e9b45178496b
54ac <me@54ac.ovh>
2024-10-10 23:59:13 +0200

    add auto update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { VitePWA } from "vite-plugin-pwa";
 export default {
 	plugins: [
 		VitePWA({
+			registerType: "autoUpdate",
 			includeAssets: ["favicon.ico"],
 			manifest: {
 				short_name: "the list picker",
